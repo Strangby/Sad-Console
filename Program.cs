b04ef3426b6e7293ccbe4e436c9d9165c865175d
Strangby <h21lied@axevalla.fhsk.se>
2024-01-24 11:48:33 +0100

    Ja

--- Program.cs
+++ Program.cs
@@ -10,8 +10,14 @@ static void Startup(object? sender, GameHost host)
 {
     Console startingConsole = Game.Instance.StartingConsole!;
 
-    Game.Instance.StartingConsole!.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole!.Font);
-    Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Purple, Color.Black, 0, Mirror.None);
-    Game.Instance.StartingConsole.Print(5, 4, "I Always Come Back");
-    startingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateBorder(new ColoredGlyph(Color.Purple, Color.Black, 176)));
+    startingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Purple, Color.Black, 0, Mirror.None);
+    startingConsole.Print(5, 4, "I Always Come Back");
+
+    startingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateStyledBox(ICellSurface.ConnectedLineThin,
+                                                        new ColoredGlyph(Color.Purple, Color.Black)));
+
+    startingConsole.DrawCircle(new Rectangle(5, 8, 16, 8), ShapeParameters.CreateFilled(new ColoredGlyph(Color.Purple, Color.Black, 176),
+                                                           new ColoredGlyph(Color.White, Color.Black)));
+
+    startingConsole.DrawLine(new Point(60, 5), new Point(66, 20), '$', Color.AnsiRed, Color.AnsiRed, Mirror.None);
 }
