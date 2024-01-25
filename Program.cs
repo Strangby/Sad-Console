fdb830db1366385b60ab75c694f4544c2825830e
unknown <h22anfr@axevalla.fhsk.se>
2024-01-25 11:52:18 +0100

    Ja

--- Program.cs
+++ Program.cs
@@ -2,22 +2,56 @@
 
 Settings.WindowTitle = "My SadConsole Game";
 
-Game.Create(90, 30, Startup);
+Builder configuration = new Builder()
+    .SetScreenSize(90, 30)
+    .OnStart(Startup)
+    ;
+
+Game.Create(configuration);
 Game.Instance.Run();
 Game.Instance.Dispose();
 
 static void Startup(object? sender, GameHost host)
 {
-    Console startingConsole = Game.Instance.StartingConsole!;
+    ScreenObject container = new ScreenObject();
+    Game.Instance.Screen = container;
 
-    startingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Purple, Color.Black, 0, Mirror.None);
-    startingConsole.Print(5, 4, "I Always Come Back");
+    // First console
+    Console console1 = new(60, 14);
+    console1.Position = (3, 2);
+    console1.Surface.DefaultBackground = Color.AnsiCyan;
+    console1.Clear();
+    console1.Print(1, 1, "Type on me!");
+    console1.Cursor.Position = (1, 2);
+    console1.Cursor.IsEnabled = true;
+    console1.Cursor.IsVisible = true;
+    console1.Cursor.MouseClickReposition = true;
+    console1.IsFocused = true;
+    console1.FocusOnMouseClick = true;
+    console1.MoveToFrontOnMouseClick = true;
 
-    startingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateStyledBox(ICellSurface.ConnectedLineThin,
-                                                        new ColoredGlyph(Color.Purple, Color.Black)));
+    container.Children.Add(console1);
 
-    startingConsole.DrawCircle(new Rectangle(5, 8, 16, 8), ShapeParameters.CreateFilled(new ColoredGlyph(Color.Purple, Color.Black, 176),
-                                                           new ColoredGlyph(Color.White, Color.Black)));
+    // Add a child surface
+    ScreenSurface surfaceObject = new(5, 3);
+    surfaceObject.Surface.FillWithRandomGarbage(surfaceObject.Font);
+    surfaceObject.Position = console1.Surface.Area.Center - (surfaceObject.Surface.Area.Size / 2);
+    surfaceObject.UseMouse = false;
 
-    startingConsole.DrawLine(new Point(60, 5), new Point(66, 20), '$', Color.AnsiRed, Color.AnsiRed, Mirror.None);
+    console1.Children.Add(surfaceObject);
+
+    // Second console
+    Console console2 = new Console(58, 12);
+    console2.Position = new Point(19, 11);
+    console2.Surface.DefaultBackground = Color.AnsiRed;
+    console2.Clear();
+    console2.Print(1, 1, "Type on me!");
+    console2.Cursor.Position = new Point(1, 2);
+    console2.Cursor.IsEnabled = true;
+    console2.Cursor.IsVisible = true;
+    console2.FocusOnMouseClick = true;
+    console2.MoveToFrontOnMouseClick = true;
+
+    container.Children.Add(console2);
+    container.Children.MoveToBottom(console2);
 }
