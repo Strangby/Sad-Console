36924dbb26e03f92858daa15666cba4678b277d0
unknown <h22anfr@axevalla.fhsk.se>
2024-02-08 11:53:16 +0100

    Ja

--- Map.cs
+++ Map.cs
@@ -25,6 +25,7 @@ internal class Map
         {
             CreateTreasure();
             CreateMonster();
+            CreateWall();
         }
     }
 
@@ -92,6 +93,21 @@ internal class Map
 
             Monster monster = new Monster(randomPosition, _mapSurface);
             _mapObjects.Add(monster);
+            break;
+        }
+    }
+    private void CreateWall()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Point randomPosition = new Point(Game.Instance.Random.Next(0, _mapSurface.Surface.Width),
+                                                Game.Instance.Random.Next(0, _mapSurface.Surface.Height));
+
+            bool foundObject = _mapObjects.Any(obj => obj.Position == randomPosition);
+            if (foundObject) continue;
+
+            Wall wall = new Wall(randomPosition, _mapSurface);
+            _mapObjects.Add(wall);
             break;
         }
     }
---
+++ Wall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadConsoleGame
+{
+    internal class Wall:GameObject
+    {
+        public Wall(Point position, IScreenSurface hostingSurface)
+        : base(new ColoredGlyph(Color.Brown, Color.Brown, 'W'), position, hostingSurface)
+        {
+
+        }
+        public override bool Touched(GameObject source, Map map)
+        {
+            return base.Touched(source, map);
+        }
+
+    }
+}
